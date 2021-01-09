0c7dd48009f9dd6d1697c3d105db55af8d468d1e
Fenix Lavon <flavon@si-g1.kzn.21-school.ru>
2021-01-09 13:22:23 +0300

    write front and back method. Start wrtie modifers method in vector

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -26,8 +26,30 @@ bool check_vector(T &my_vector, STD &orig_vector) {
 }
 
 int main() {
+	/* constuctor check */
 	{
-		std::vector<int> orig_vect;
-		ft::vector<int> ft_vect;
+		size_t size = 5;
+		ft::vector<int> vector1(size, 10);
+		ft::vector<int> vector2(vector1);
+		print_vector(vector2);
+		ft::vector<double> vector3;
+		ft::vector<double> vector4;
+		vector4 = vector3;
+		ft::vector<int>::iterator it = vector1.begin();
+		ft::vector<int>::iterator ite = vector1.end();
+		ft::vector<int> vector5(it, ite);
+		print_vector(vector5);
+	}
+	{
+		ft::vector<int> my_vector;
+		std::vector<int> orig_vector;
+		for (int i = 1; i < 100; ++ i)
+			my_vector.push_back(i);
+		for (int i = 1; i < 100; ++ i)
+			orig_vector.push_back(i);
+		if (check_vector(my_vector, orig_vector))
+			std::cout << "OK" << std::endl;
+		std::cout << "My capacity: " << my_vector.capacity() << std::endl;
+		std::cout << "Orig capacity: " << orig_vector.capacity() << std::endl;
 	}
 }
--- vector.hpp
+++ vector.hpp
@@ -15,9 +15,10 @@ class ft::vector {
 public:
 	typedef T 					value_type;
 	typedef Alloc 				allocator_type;
-	typedef value_type& 		reference;
-	typedef const value_type	&const_reference;
-	typedef value_type *		pointer;
+	typedef typename allocator_type::reference reference;
+	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::pointer pointer;
+	typedef typename allocator_type::const_pointer const_pointer;
 	typedef size_t 				size_type;
 	typedef ptrdiff_t 			difference_type;
 
@@ -25,37 +26,51 @@ private:
 	pointer _arr;
 	allocator_type _alloc;
 	size_type _size;
+	size_type _capacity;
 
-	pointer createVector(size_type size, const_reference val) {
-		pointer arr = _alloc.allocate(size);
-		for (size_type i = 0; i < size; ++i)
+	pointer createVector(const_reference val) {
+		pointer arr = _alloc.allocate(_capacity);
+		for (size_type i = 0; i < _size; ++i)
 			_alloc.construct(arr + i, val);
 		return arr;
+	}
+
+	void reallocVector() {
+		_capacity = _size * 2;
+		pointer arr = _alloc.allocate(_capacity);
+		for (size_type i = 0; i < _size; ++i)
+			_alloc.construct(arr + i, *(_arr + i));
+		clear();
+		_arr = arr;
 	}
 
 public:
 	/* Constructor */
 	explicit vector (const allocator_type& alloc = allocator_type())
-		: _alloc(alloc) { _arr = createVector(0, value_type()); };
+		: _alloc(alloc), _size(0), _capacity(1) { _arr = createVector(value_type()); };
 	explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-		: _alloc(alloc), _size(n) { _arr = createVector(_size, val); };
+		: _alloc(alloc), _size(n), _capacity(_size) { _arr = createVector(val); };
 	template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
-		: _alloc(alloc) {
-		createVector(0, value_type());
+		: _alloc(alloc), _size(0), _capacity(1) {
+		_arr = createVector(value_type());
 		while (first != last) {
 			push_back(*first);
 			first++;
 		}
 	};
 
-	vector (const vector& x) { *this = x; };
+	vector (const vector& x) : _alloc(x._alloc), _size(x._size) {
+		_arr = _alloc.allocate(_size);
+		for (size_type i = 0; i < _size; ++i)
+			_alloc.construct(_arr + i, *(x._arr + i));
+	};
 
 	/* Destructor */
 	~vector() { clear(); };
 
 	/* Assignation operator */
 	vector& operator= (const vector& x) {
-		if (_arr != nullptr)
+		if (_size != 0)
 			clear();
 		_size = x._size;
 		_alloc = x._alloc;
@@ -71,7 +86,7 @@ public:
 		pointer _it;
 	public:
 		explicit iterator(pointer it = nullptr) : _it(it) {};
-		~iterator() { delete [] this->_it; };
+		~iterator() {};
 		iterator & operator=(const iterator &it) {
 			if (this->_it != nullptr)
 				delete [] this->_it;
@@ -106,7 +121,7 @@ public:
 		bool operator>=(const iterator &other) const { return _it >= other.getElem(); };
 		bool operator<(const iterator &other) const { return _it < other.getElem(); };
 		bool operator>(const iterator &other) const { return _it > other.getElem(); };
-		pointer getElem() { return this->_it; }
+		pointer getElem() const { return this->_it; }
 	};
 
 	/* const iterator */
@@ -164,33 +179,42 @@ public:
 	const_iterator begin() const { return const_iterator(this->_arr); };
 	iterator end() { return iterator(this->_arr + this->_size); };
 	const_iterator end() const { return const_iterator(this->_arr + this->_size); };
-	reverse_iterator rbegin();
-	const_reverse_iterator rbegin() const;
-	reverse_iterator rend();
-	const_reverse_iterator rend() const;
+	reverse_iterator rbegin() { return reverse_iterator(this->_arr + this->_size - 1); };
+	const_reverse_iterator rbegin() const { return const_reverse_iterator(this->_arr + this->_size - 1); };
+	reverse_iterator rend() { reverse_iterator(this->_arr); };
+	const_reverse_iterator rend() const { const_reverse_iterator(this->_arr); };
 
 	/* Capacity */
 	size_type size() const { return _size; };
 	size_type max_size() const { return std::numeric_limits<size_type>::max() / sizeof(pointer); };
 	void resize (size_type n, value_type val = value_type());
-	size_type capacity() const;
+	size_type capacity() const { return _capacity; };
 	bool empty() const { return _arr == nullptr; };
 	void reserve (size_type n);
 
 	/* Element access */
-	reference operator[] (size_type n);
-	const_reference operator[] (size_type n) const;
+	reference operator[] (size_type n) {
+		return *(_arr + n);
+	};
+	const_reference operator[] (size_type n) const {
+		return *(_arr + n);
+	};
 	reference at (size_type n);
 	const_reference at (size_type n) const;
-	reference front();
-	const_reference front() const;
-	reference back();
-	const_reference back() const;
+	reference front() { return *_arr; };
+	const_reference front() const { return *_arr; };
+	reference back() { return *(_arr + _size - 1); };
+	const_reference back() const { return *(_arr + _size - 1); };
 
 	/* Modifiers */
 	template <class InputIterator> void assign (InputIterator first, InputIterator last);
 	void assign (size_type n, const value_type& val);
-	void push_back (const value_type& val);
+	void push_back (const value_type& val) {
+		if (_size == _capacity)
+			reallocVector();
+		_alloc.construct(_arr + _size, val);
+		_size++;
+	};
 	void pop_back();
 	iterator insert (iterator position, const value_type& val);
 	void insert (iterator position, size_type n, const value_type& val);
